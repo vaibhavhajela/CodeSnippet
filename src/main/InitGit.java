ea52a61976d14bb0b12e231fae6a67f5af6083ce
vaibhavhajela <vaibhav.2405@gmail.com>
2021-01-02 11:00:09 +0530

    Update InitGit.java

--- src/main/InitGit.java
+++ src/main/InitGit.java
@@ -7,4 +7,5 @@ public class Testrepoclass{ //changes done by Sujoy
 // Testing Fetch and merge   // Changes done by apoorva
 
 // Testing Fetch and merge - // changes done by vaibhav
+//Testing master branch protection
 
